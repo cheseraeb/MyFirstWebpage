3948737bab6096f6dd94c6836c99ab28342690d9
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-05-28 14:50:27 +0200

    learning javascript

--- JavaScripts/firstscript.js
+++ JavaScripts/firstscript.js
@@ -1,1 +1,20 @@
-alert("Hello Cheeze!!!")
+// *********************************************************
+// Author : Cheserae Bidgood(Beukes)
+//    Date : 28/05/2018  
+// *********************************************************
+
+// Basic Syntax and structure for JAVAScript
+// Rule 1. CASE SENSATIVE
+// Rule 2. most things in Java is statements and each end in a semi colon
+// Rule 3. Add comments
+// Rule 4. Script runs from top to bottom
+
+// Variables************************************************
+var myVariable_1;
+var myVariable_2;
+var myVariable_3;
+var myVariable_4;
+
+
+
+
