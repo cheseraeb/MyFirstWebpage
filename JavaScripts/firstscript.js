3cbb428a55061e9d503775408102c80125dd8c80
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-05-28 15:55:04 +0200

    Java Training

--- JavaScripts/firstscript.js
+++ JavaScripts/firstscript.js
@@ -10,10 +10,46 @@
 // Rule 4. Script runs from top to bottom
 
 // Variables************************************************
-var myVariable_1;
-var myVariable_2;
-var myVariable_3;
-var myVariable_4;
+// Dont have to specify variable types
+// String
+var myStringVariable_1;
+// Number
+var myNumberVariable_1;
+// Boolean
+var myBooleanVariable_1;
+var myBooleanVariable_2;
+// Array
+var myArrayVariable_1;
+// *********************************************************
+// document.write (Writes to the web page)
+// console.log (writes to the console)
+// {} is called a code block
+myBooleanVariable_1 = true;
+myNumberVariable_1 = 10;
+
+if (myBooleanVariable_3){
+    document.write("CHEEEEEEEEEEEEZZZZZZZZZZZZZZEEEEEEEEEE!!!!!!!!!!!");
+} else{
+    document.write("Hello Motto!!")
+}
+if (myNumberVariable_2 > 9){
+    document.write("Hellow Cheeze!!!");
+} else{
+    document.write("leave me alone")
+}
+
+myBooleanVariable_2 = false;
+
+if (myBooleanVariable_2){
+    document.write("Hellow me");
+
+} else if (myBooleanVariable_1){
+    document.write("Hello you!");
+
+} else {
+    document.write("Hello US!!!!");
+
+}
 
 
 
