46bd10e6bd5f57794236cdeaf9b210fb9a8a1084
Cheserae Beukes <cheseraeb@telesure.co.za>
2018-05-30 07:48:20 +0200

    All

--- JavaScripts/DOM.js
+++ JavaScripts/DOM.js
@@ -2,3 +2,7 @@
 // Author : Cheserae Bidgood(Beukes)
 //    Date : 30/05/2018  
 // *********************************************************
+function DOM(){
+    var price_class = document.getElementsByClassName("cheese_info");
+    var price_change = document.getElementsByTagName("");
+}
